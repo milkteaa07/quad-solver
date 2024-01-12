0bf2a958ec4f8f212c2f1c96ef3c6d35da9caf15
Ava Teng <ateng25@students.polytechnic.org>
2024-01-12 14:35:16 -0800

    added quad formula

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -1,5 +1,6 @@
 public class QuadraticSolver{
     public static void main(String[] args){
         System.out.println("Welcome to the Quadratic solver!");
+        System.out.println("The quadratic formula is (x = (-b +- sqrt(b^2-4ac))/2a)");
     }
 }
